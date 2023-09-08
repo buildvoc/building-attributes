b0972052ab2203af59049214861a6a23539bd9ad
Latief Anwar <latifulanwar@gmail.com>
2023-09-09 04:44:11 +0700

    update SxData, save query

--- app/Http/Controllers/API/SxDataController.php
+++ app/Http/Controllers/API/SxDataController.php
@@ -17,6 +17,11 @@ class SxDataController extends Controller
      */
     public function index()
     {
+        // with t as (SELECT st_transform( st_setsrid( ST_MakePoint("easting","northing"),27700),4326) geom ,* from osopentoid)
+        // SELECT ST_X (ST_Transform (geom, 4326)) AS long, ST_Y (ST_Transform (geom, 4326)) AS lat,* from t
+        // LEFT JOIN sx_data sd on toid = sd.os_topo_toid
+        // WHERE ST_DWithin(geom, ST_SetSRID(ST_Point(-0.7995479,51.2139666), 4326), 10)
+
         return ApiJsonResponse::sendOkResponse(['sx_data' => SxData::all()]);
     }
 }
