f4628c75e3c37f5389d7814f88f7565f156c2709
Maher El Gamil <maherbusnes@gmail.com>
2023-09-11 15:05:56 +0300

    WIP: osdata form

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,35 @@
 import './bootstrap';
 import * as osdatahub from "osdatahub";
 
-const apiKey = "GE4Viu5Q2kB6jnFzoPnr3uGXhGwmm3iZ"
+const apiKey = import.meta.env.VITE_OS_API_KEY;
+const collectionId = import.meta.env.VITE_OS_COLLECTION_ID;
 
-async function find() {
-    axios.get('https://api.os.uk/search/names/v1/find?query=Southampton&key=' + apiKey)
+
+// get input with query id
+const queryInput = document.getElementById('query');
+const searchForm = document.getElementById('search-form');
+
+// add event listener to search button
+searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const query = queryInput.value;
+
+    osdatahub.ngd.features(apiKey, collectionId, {filter: query})
         .then(function (response) {
-            /* For explanation and debugging purposes we display the full response from the API in the console */
-            console.log(JSON.stringify(response.data, null, 2));
-        });
-}
+            console.log(response);
 
-find();
+            // fill table with results
+        })
+        .catch(function (error) {
+            alert(error);
+        });
+});
+
+// async function find(query)
+// {
+//     axios.get(`https://api.os.uk/search/names/v1/find?query=${query}&key=${apiKey}`)
+//         .then(function (response) {
+//             /* For explanation and debugging purposes we display the full response from the API in the console */
+//             console.log(JSON.stringify(response.data, null, 2));
+//         });
+// }
--- resources/views/osdata.blade.php
+++ resources/views/osdata.blade.php
@@ -8,8 +8,27 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-<main>
+<form
+    class="flex flex-col items-center justify-center min-h-screen bg-gray-100"
+    id="search-form"
+>
+    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">
+        OS NGD API – Features - <b>{{ env('VITE_OS_COLLECTION_ID') }} collection</b>
+    </label>
+    <div class="mt-2 flex rounded-md shadow-sm">
+        <div class="relative flex flex-grow items-stretch focus-within:z-10">
+            <input
+                type="text"
+                name="query"
+                id="query"
+                class="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Search" />
+        </div>
+        <button type="submit" class="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+            <span>Search</span>
+        </button>
+    </div>
+</form>
 
-</main>
 </body>
 </html>
