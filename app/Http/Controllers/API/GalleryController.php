fd22ba251860bc44106a2fad13c0d299a97502a8
Latief Anwar <latifulanwar@gmail.com>
2024-01-31 15:20:43 +0700

    sync gallery

--- app/Http/Controllers/API/GalleryController.php
+++ app/Http/Controllers/API/GalleryController.php
@@ -119,6 +119,14 @@ class GalleryController extends Controller
                     'url' => 'https://buildingshistory.co.uk/galleries/' . $gallery->slug
                 ]
             );
+            DB::connection('brick')->table('collections_task')->updateOrCreate(
+                ['collection_id' => $gallery->id],
+                [
+                    'organization_id' => 'nypl',
+                    'collection_id' => $gallery->id,
+                    'task_id' => 'geotag-photo'
+                ]
+            );
         }
 
         $collections = DB::connection('brick')->table('collections')->get();
